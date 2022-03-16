533631671340c9a3ee0561c33998729ac632a64a
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2022-03-15 23:01:47 -0400

    coding challaenge complete

---
+++ main.js
@@ -0,0 +1,7 @@
+// Write a function which converts the input string to uppercase.
+// Fundamentals
+function makeUpperCase(str) {
+    // Code here 
+    return str.toUpperCase()
+  }
+  console.log(makeUpperCase("hi how are you"))
